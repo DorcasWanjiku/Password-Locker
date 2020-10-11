e8826d5b09227c71f9e9cc424755d45a9503ca59
DorcasWanjiku <smallwanjiku@gmail.com>
2020-10-11 10:31:23 +0300

    add new codes and one deletion

--- account_test.py
+++ account_test.py
@@ -41,3 +41,47 @@ class TestAccount(unittest.TestCase):
             test_account = Account("Test","user","0714152325","test@user.com") 
             test_account.save_account()
             self.assertEqual(len(Account.account_list),2)
+             def test_delete_account(self):
+            '''
+            test_delete_account to test if we can remove an account from our account list
+            '''
+            self.new_account.save_account()
+            test_account = Account("Test","user","0714152325","test@user.com") 
+            test_account.save_account()
+
+            self.new_account.delete_account()
+            self.assertEqual(len(Account.account_list),1)        
+     
+    def test_find_account_by_account_name(self):
+        '''
+        test to check if we can find an account by account_name and display information
+        '''
+
+        self.new_account.save_account()
+        test_account = Account("Test","user","0741421079","test@user.com") # new account
+        test_account.save_account()
+
+        found_account = Account.find_by_name("Test")
+
+        self.assertEqual(found_account.email,test_account.email)    
+    def test_account_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the account.
+        '''
+
+        self.new_account.save_account()
+        test_account = Account("Test","user","0741421079","test@user.com") # new account
+        test_account.save_account()
+
+        account_exists = Account.account_exist("0741421079")
+
+        self.assertTrue(account_exists)
+    def test_display_all_accounts(self):
+        '''
+        method that returns a list of all accounts saved
+        '''
+        displayed = Account.display_accounts()
+        self.assertEqual(displayed,Account.account_list)    
+
+if __name__ == '__main__':
+    unittest.main()

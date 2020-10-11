2aa430363f1f94e40ae3ff2ab1ade0f977e4d696
DorcasWanjiku <smallwanjiku@gmail.com>
2020-10-11 10:25:00 +0300

    add changes codes and some deletion

--- account_test.py
+++ account_test.py
@@ -12,7 +12,7 @@ class TestAccount(unittest.TestCase):
         '''
         test_init test case to test if the object is initialized properly
         '''
-        self.assertEqual(self.new_account.account_name,"Ahmed")
-        self.assertEqual(self.new_account.user_name,"Mukhtar")
-        self.assertEqual(self.new_account.password,"abc234")
-        self.assertEqual(self.new_account.email,"mukhtarabdirahman@gmail.com")
+        self.assertEqual(self.new_account.account_name,"Dorcas")
+        self.assertEqual(self.new_account.user_name,"Wanjiku")
+        self.assertEqual(self.new_account.password,"sh3k1l33")
+        self.assertEqual(self.new_account.email,"smallwanjiku@gmail.com")
